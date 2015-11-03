2e524a3ed1aa415202ff6b8b355723abcfd4ea76
Allan IRDEL <allan.irdel@gmail.com>
2015-11-03 05:24:26 +0100

    +menu

---
+++ src/WebBundle/EventListener/MenuItemListListener.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Allan
+ * Date: 03/11/2015
+ * Time: 05:14
+ */
+
+namespace WebBundle\EventListener;
+
+use Avanzu\AdminThemeBundle\Event\SidebarMenuEvent;
+use Avanzu\AdminThemeBundle\Model\MenuItemModel;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuItemListListener{
+
+    /**
+     * @param SidebarMenuEvent $event
+     */
+    public function onSetupMenu(SidebarMenuEvent $event) {
+
+        $request = $event->getRequest();
+
+        foreach ($this->getMenu($request) as $item) {
+            $event->addItem($item);
+        }
+
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    protected function getMenu(Request $request) {
+        // Build your menu here by constructing a MenuItemModel array
+        $menuItems = array(
+            $dashboard = new MenuItemModel('dashboard', 'Home', 'app_homepage', array(), 'fa fa-laptop'),
+        );
+
+        return $this->activateByRoute($request->get('_route'), $menuItems);
+    }
+
+    /**
+     * @param $route
+     * @param $items
+     * @return mixed
+     */
+    protected function activateByRoute($route, $items) {
+
+        foreach($items as $item) {
+            if($item->hasChildren()) {
+                $this->activateByRoute($route, $item->getChildren());
+            }
+            else {
+                if($item->getRoute() == $route) {
+                    $item->setIsActive(true);
+                }
+            }
+        }
+
+        return $items;
+    }
+
+}
