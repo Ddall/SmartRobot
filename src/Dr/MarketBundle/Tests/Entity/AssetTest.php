edacfbf40a72431a893f8a1fea0ad7b4d3e5b864
Allan IRDEL <allan.irdel@gmail.com>
2015-12-13 21:04:00 +0100

    noteststocallmyown :-(

--- src/Dr/MarketBundle/Tests/Entity/AssetTest.php
+++ src/Dr/MarketBundle/Tests/Entity/AssetTest.php
@@ -6,7 +6,9 @@
 
 namespace Dr\MarketBundle\Tests\Entity;
 
-
 class AssetTest extends \PHPUnit_Framework_TestCase {
 
+    public function testSetType(){
+        $this->assertTrue(true);
+    }
 }
