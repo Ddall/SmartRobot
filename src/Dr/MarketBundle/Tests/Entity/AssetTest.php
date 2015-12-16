43df5ff8a5c81a67de3efe3f6def62fcf81fd338
Allan IRDEL <allan.irdel@gmail.com>
2015-12-16 03:29:41 +0100

    wip assetTests

--- src/Dr/MarketBundle/Tests/Entity/AssetTest.php
+++ src/Dr/MarketBundle/Tests/Entity/AssetTest.php
@@ -10,12 +10,49 @@ use Dr\MarketBundle\Entity\Asset;
 
 class AssetTest extends \PHPUnit_Framework_TestCase {
 
-    public function testContructor(){
+    public function testConstructor(){
 
         $asset = new Asset('Euro', 'EUR', '€', Asset::TYPE_FIAT);
 
-        $this->assertTrue( $asset instanceof Asset );
-        $this->assertTrue( $asset->getName() === 'Euro' );
+        $this->assertInstanceOf('Dr\MarketBundle\Entity\Asset', $asset, 'Asset contructor failed to create instance of Asset');
+
+        $this->assertEquals('Euro', $asset->getName(), 'Asset constructor did not set Name properly');
+
+        $this->assertEquals(Asset::TYPE_FIAT, $asset->getType(), 'Asset constructor did not set type properly');
+
+        $this->assertEquals('€', $asset->getDisplaySymbol(), 'Asset contructor did not set symbol properly');
+
+        unset($asset);
+        $this->assertEquals(Asset::TYPE_UNDEFINED, $asset->getType(), 'Asset constructor failed to set default type');
+
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testTypeException(){
+        $asset = new Asset();
+
+        $asset->setType('toto');
+
+    }
+
+    public function testType(){
+
+        $asset = new Asset();
+        $asset->setType(Asset::TYPE_FIAT);
+        $this->assertEquals(Asset::TYPE_FIAT, $asset->getType());
+
+        $this->assertStringEndsWith(')', $asset->__toString(), 'Asset __toSting may have a problem');
+
+        $this->assertContains('Fiat', $asset->getTypeString(), 'Asset typeString may have a problem');
+
+        $asset->setType(Asset::TYPE_VIRTUAL);
+        $this->assertContains('Virtual', $asset->getTypeString(), 'Asset typeString may have a problem');
+
+        $asset->setType(Asset::TYPE_UNDEFINED);
+        $this->assertContains('Undefined', $asset->getTypeString(), 'Asset typeString may have a problem');
 
     }
 }
