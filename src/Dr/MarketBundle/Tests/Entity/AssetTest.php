d77d332f65f7e52af34703ac533d9c3b7f4bf6f4
Allan IRDEL <allan.irdel@gmail.com>
2015-12-16 02:55:52 +0100

    update assetTest

--- src/Dr/MarketBundle/Tests/Entity/AssetTest.php
+++ src/Dr/MarketBundle/Tests/Entity/AssetTest.php
@@ -6,9 +6,16 @@
 
 namespace Dr\MarketBundle\Tests\Entity;
 
+use Dr\MarketBundle\Entity\Asset;
+
 class AssetTest extends \PHPUnit_Framework_TestCase {
 
-    public function testSetType(){
-        $this->assertTrue(true);
+    public function testContructor(){
+
+        $asset = new Asset('Euro', 'EUR', '€', Asset::TYPE_FIAT);
+
+        $this->assertTrue( $asset instanceof Asset );
+        $this->assertTrue( $asset->getName() === 'Euro' );
+
     }
 }
