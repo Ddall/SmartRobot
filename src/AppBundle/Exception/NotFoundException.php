09b910038d942d402cb829eb962fdc4b65617ef5
Allan IRDEL <allan.irdel@gmail.com>
2015-12-17 18:12:25 +0100

    add custom exceptions

---
+++ src/AppBundle/Exception/NotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * DdxSr - NotFoundException.php
+ * Created by Allan.
+ */
+
+namespace AppBundle\Exception;
+
+
+class NotFoundException extends BaseException{
+
+}
---
+++ src/AppBundle/Exception/WrongTypeException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * DdxSr - WrongTypeException.php
+ * Created by Allan.
+ */
+
+namespace AppBundle\Exception;
+
+
+class WrongTypeException extends BaseException{
+
+}
